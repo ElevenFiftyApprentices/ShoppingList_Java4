5c859a30ce1c91a2f0a5876cd0c92952561c2d11
Turner-Tobie <turner.tobie@yahoo.com>
2016-11-29 11:27:42 -0500

    update to file structure

---
+++ src/main/java/org/elevenfifty/shoppinglist/controller/IndexController.java
@@ -0,0 +1,13 @@
+package org.elevenfifty.shoppinglist.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class IndexController {
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+}
