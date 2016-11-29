2ae4f611d32723ed3928836bd51be0d96ca9f9ff
jonathaneyster <jonathaneyster@gmail.com>
2016-11-29 15:11:46 -0500

    added userrepo

---
+++ src/main/java/org/elevenfifty/shoppinglist/repositories/UserRepository.java
@@ -0,0 +1,21 @@
+package org.elevenfifty.shoppinglist.repositories;
+
+import java.util.List;
+
+import org.elevenfifty.shoppinglist.beans.User;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+	List<User> findByEmail(String email);
+
+	List<User> findAllByOrderByFirstNameAscLastNameAsc();
+
+	List<User> findByLastNameOrFirstNameOrEmailOrTwitterHandleOrFacebookUrlIgnoreCase(
+			String lastName, String firstName, String email, String twitterHandle,
+			String facebookUrl);
+
+}
