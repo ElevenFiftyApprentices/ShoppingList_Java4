14ec34ff0c6e886c411d43e76c4cdb81dd538c51
Turner-Tobie <turner.tobie@yahoo.com>
2016-11-30 16:57:52 -0500

    list items html

--- src/main/java/org/elevenfifty/shoppinglist/controller/ShoppingListController.java
+++ src/main/java/org/elevenfifty/shoppinglist/controller/ShoppingListController.java
@@ -4,8 +4,6 @@ import javax.validation.Valid;
 
 import org.elevenfifty.shoppinglist.beans.ShoppingList;
 import org.elevenfifty.shoppinglist.repositories.ShoppingListRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
