cffb7b4002ac936b58699f9b65ab933b62f7a518
Turner-Tobie <turner.tobie@yahoo.com>
2016-12-02 16:15:47 -0500

    setup list create to link to list desc. page

--- src/main/java/org/elevenfifty/shoppinglist/controller/ShoppingListController.java
+++ src/main/java/org/elevenfifty/shoppinglist/controller/ShoppingListController.java
@@ -1,36 +1,27 @@
 package org.elevenfifty.shoppinglist.controller;
 
-import java.security.Permission;
-import java.util.List;
+import javax.validation.Valid;
 
 import org.elevenfifty.shoppinglist.beans.ShoppingList;
-import org.elevenfifty.shoppinglist.beans.User;
-import org.elevenfifty.shoppinglist.beans.UserRole;
 import org.elevenfifty.shoppinglist.repositories.ShoppingListRepository;
 import org.elevenfifty.shoppinglist.repositories.UserRepository;
 import org.elevenfifty.shoppinglist.security.PermissionService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
-import static org.elevenfifty.shoppinglist.security.Role.ROLE_USER;
-import static org.h2.util.StringUtils.isNullOrEmpty;
 
 @Controller
 public class ShoppingListController {
-	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
-
+	private static final Logger log = LoggerFactory.getLogger(ShoppingListController.class);
+	
 	@Autowired
 	private ShoppingListRepository shoppingListRepo;
 	
@@ -42,11 +33,33 @@ public class ShoppingListController {
 
 	@Secured("ROLE_USER")
 	@RequestMapping("/shoppingLists")
-	public String ShoppingList(Model model)
+	public String shoppingLists(Model model)
 	{
 		long currentUserId = permissionService.findCurrentUserId(); 
 		model.addAttribute("shoppingList", shoppingListRepo.findAllById(currentUserId));
 		return "shoppingList/shoppingLists";
+	}
+	
+	@Secured("ROLE_USER")
+	@RequestMapping("/contact/{contactId}")
+	public String shoppingList(@PathVariable long contactId, Model model) {
+		model.addAttribute("contact", shoppingListRepo.findOne(contactId));
+		model.addAttribute("permissions", permissionService);
+		return "shoppingList/shoppingList";
+	}
+	
+	@RequestMapping(value = "/shoppingList/{shoppingListId}/shoppingList_edit", method = RequestMethod.GET)
+	public String editShoppingList(@PathVariable long shoppingListId, Model model){
+		model.addAttribute("shoppingList", shoppingListRepo.findOne(shoppingListId));
+		return "shoppingList_edit";
+	}
+	
+	@RequestMapping(value = "/shoppingList/{shoppingListId}/shoppingList_edit", method = RequestMethod.POST)
+	public String shoppingListSave(@ModelAttribute ShoppingList shoppingList, @PathVariable long shoppingListId, Model model){
+		log.debug("Saving Shopping List" + shoppingList);
+		shoppingListRepo.save(shoppingList);
+		model.addAttribute("message", "Contact " + shoppingList.getEmail() + " saved.");
+		return shoppingList(shoppingListId, model);
 	}
 	@RequestMapping(value = "/shoppingList/shoppingList_create", method = RequestMethod.GET)
 	public String createShoppingList(Model model) {
@@ -55,15 +68,16 @@ public class ShoppingListController {
 		return "shoppingList/shoppingList_create";
 	}
 	
+	
+	
 	@RequestMapping(value = "/shoppingList/shoppingList_create", method = RequestMethod.POST)
-	public String createShoppingList(@ModelAttribute ShoppingList shoppingList,
+	public String createShoppingList(@ModelAttribute @Valid ShoppingList shoppingList,
 	    Model model) {
 
 		log.info(shoppingList.toString());
-		
 		ShoppingList savedShoppingList = shoppingListRepo.save(shoppingList);
 		
-		return "shoppingList/shoppingList_create";
+		return shoppingListSave(savedShoppingList, savedShoppingList.getId(), model);
 	}
 
 
