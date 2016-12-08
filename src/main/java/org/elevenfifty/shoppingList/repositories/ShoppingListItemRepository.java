87ec441795e3b04b1adb805e5b9c6cc5fce83585
Turner-Tobie <turner.tobie@yahoo.com>
2016-12-08 12:14:39 -0500

    added redirect for the website at launch

--- src/main/java/org/elevenfifty/shoppingList/repositories/ShoppingListItemRepository.java
+++ src/main/java/org/elevenfifty/shoppingList/repositories/ShoppingListItemRepository.java
@@ -3,7 +3,9 @@ package org.elevenfifty.shoppinglist.repositories;
 import org.elevenfifty.shoppinglist.beans.ShoppingListItem;
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface ShoppingListItemRepository extends CrudRepository<ShoppingListItem, Long> {
 	
-
+	ShoppingListItem findByShoppingListIdAndId(long shoppingListId, long id);
 }
